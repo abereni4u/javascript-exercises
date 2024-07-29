069b9fc29f23a49fee47ea4249c1a867c35499ad
abereni4u <aberenijopuiyo@gmail.com>
2024-07-29 03:13:40 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    for(let i = 0; i < string.length; i++){
+        newString = string[i] + newString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
